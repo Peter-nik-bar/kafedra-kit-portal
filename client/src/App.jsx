e97a51db467c41774a9e8eb3981531de295db53e
Peter-nik-bar <peter.nik.bar@gmail.com>
2026-05-06 16:58:25 +0200

    Виправлено ScrollToTop – прокручування на початок сторінки при переході

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,15 @@
-import { Routes, Route } from 'react-router-dom'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import Home from './pages/Home'
-import About from './pages/About'
-import Gallery from './pages/Gallery'
-import News from './pages/News'
-import Contacts from './pages/Contacts'
-import NewsDetail from './pages/NewsDetail'
-import Admissions from './pages/Admissions'
-import ScrollToTop from './components/ScrollToTop'
+import { Routes, Route } from 'react-router-dom';
+import ScrollToTop from './components/ScrollToTop';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import About from './pages/About';
 import Staff from './pages/Staff';
+import Admissions from './pages/Admissions';
+import Gallery from './pages/Gallery';
+import News from './pages/News';
+import NewsDetail from './pages/NewsDetail';
+import Contacts from './pages/Contacts';
 
 function App() {
   return (
@@ -20,17 +20,17 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/staff" element={<Staff />} />
+          <Route path="/admissions" element={<Admissions />} />
           <Route path="/gallery" element={<Gallery />} />
           <Route path="/news" element={<News />} />
           <Route path="/news/:id" element={<NewsDetail />} />
           <Route path="/contacts" element={<Contacts />} />
-          <Route path="/admissions" element={<Admissions />} />
-	  <Route path="/staff" element={<Staff />} />
         </Routes>
       </main>
       <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
